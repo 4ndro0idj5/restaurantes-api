59727b4254a731318cd8b6b0076aea17e7431801
4ndro0idj5 <4ndro0idj5@gmail.com>
2025-06-25 16:06:20 -0300

    chore: Base do projeto criada através do Spring Initializr

---
+++ src/main/java/io/github/restaurantes_api/RestaurantesApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.restaurantes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantesApiApplication.class, args);
+	}
+
+}
