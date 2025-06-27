a859edb04c39cdf9e26b9b3d6c13d6b3701db935
4rgo11o <andersonged@yahoo.com.br>
2025-06-27 19:13:16 -0300

    feat: Criação da feature adicionar prato.

---
+++ src/main/java/io/github/restaurantes_api/dto/ItemDTO.java
@@ -0,0 +1,15 @@
+package io.github.restaurantes_api.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ItemDTO {
+    private String nome;
+    private String descricao;
+    private double preco;
+    private String foto;
+    private Long proprietarioId;
+}
+
---
+++ src/main/java/io/github/restaurantes_api/mapper/ItemMapper.java
@@ -0,0 +1,20 @@
+package io.github.restaurantes_api.mapper;
+
+import io.github.restaurantes_api.dto.ItemDTO;
+import io.github.restaurantes_api.entities.Item;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemMapper {
+
+    public ItemDTO toItemDTO(Item item){
+        return ItemDTO.builder()
+                .nome(item.getNome())
+                .descricao(item.getDescricao())
+                .preco(item.getPreco())
+                .foto(item.getFoto())
+                .proprietarioId(item.getRestaurante().getProprietarioId())
+                .build();
+
+    }
+}
